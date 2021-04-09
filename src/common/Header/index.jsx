f69d558e4a48b44b53d6e98b657281da740e9017
Lucas Torres <casteluc.dev@gmail.com>
2021-04-09 20:03:08 -0300

    chore: add react-router-dom dependencies

--- src/common/Header/index.jsx
+++ src/common/Header/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import {ReactComponent as LogoImage} from '../../assets/logo.svg'
 
@@ -11,8 +12,12 @@ const Home = () => {
             </div>
             <nav>
                 <ul>
-                    <li>Perfil</li>
-                    <li>Início</li>
+                    <li>
+                        <Link>Perfil</Link>
+                    </li>
+                    <li>
+                        <Link>Início</Link>
+                    </li>
                 </ul>
             </nav>
         </header>
