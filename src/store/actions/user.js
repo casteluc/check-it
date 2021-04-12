307aeaa37f4b3004810e2fc6f6362e888dc056ce
Lucas Torres <casteluc.dev@gmail.com>
2021-04-12 15:02:18 -0300

    refactor: remove unused dependencies

--- src/store/actions/user.js
+++
@@ -1,9 +0,0 @@
-export const signIn = (credentials) => ({
-    type: 'user/signIn',
-    payload: credentials
-})
-
-export const signUp = (credentials) => ({
-    type: 'user/signUp',
-    payload: credentials
-})
