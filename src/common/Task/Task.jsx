2f269a951bf77853a96c43ab2e7bd5ecd45dd7ac
Lucas Torres <casteluc.dev@gmail.com>
2021-04-13 01:32:08 -0300

    fix(tasks): fix task content error

--- src/common/Task/Task.jsx
+++ src/common/Task/Task.jsx
@@ -6,30 +6,33 @@ import {ReactComponent as TrashImage} from '../../assets/trash.svg'
 import {ReactComponent as CheckImage} from '../../assets/check.svg'
 
 import { TaskContainer, Button} from './style'
+import { useState } from 'react'
 
-const Task = (props) => {
+const Task = ({task, ...props}) => {
+    const [content, setContent] = useState(task.content)
+
     const handleEdit = (e) => {
-        props.editTask({targetId: props.task.id, content: e.target.innerHTML})
+        props.editTask({targetId: task.id, content})
     }
 
     const handleDelete = (e) => {
-        props.deleteTask({targetId: props.task.id})
+        props.deleteTask({targetId: task.id})
     }
     
     const handleComplete = (e) => {
-        props.completeTask({targetId: props.task.id})
+        props.completeTask({targetId: task.id})
     }
     
     return (
         <TaskContainer>
-            <p spellcheck="false" contentEditable onBlur={handleEdit} suppressContentEditableWarning={true}>{props.task.content}</p>
+            <p spellcheck="false" contentEditable onBlur={handleEdit} suppressContentEditableWarning={true}>{content}</p>
 
             <div className="buttons">
                 <Button className="delete-button" onClick={handleDelete}>
                     <TrashImage/>
                 </Button>
 
-                <Button className="checkbox" completed={props.task.completed} onClick={handleComplete}>
+                <Button className="checkbox" completed={task.completed} onClick={handleComplete}>
                     <CheckImage/>
                 </Button>
             </div>
